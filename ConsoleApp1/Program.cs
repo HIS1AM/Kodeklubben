aad55472a0f7a0db1d35ecc892a15bb2e1f1c360
Mouhamad Hisham Bakri Radwan Mouhaffel <pshisham8@gmail.com>
2022-12-01 13:05:55 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace HelloWorld
+{
+    class Kodeklubben
+    {
+        static void oppgave1()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void oppgave2()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+        static void oppgave3(string Abdi, string abo)
+        {
+            Console.WriteLine(Abdi);
+            Console.WriteLine(abo);
+        }
+        static int oppgave4(int tall1, int tall2)
+        {
+            return tall1 + tall2;
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(oppgave4(6,1));
+        }
+    }
+}
